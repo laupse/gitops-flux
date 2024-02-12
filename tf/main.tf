dd976fc4d9e5a100683c83842a445e27aa24a708
laupse <lgrondin@ingeniance.fr>
2024-02-12 11:09:17 +0100

    Fix terraform

--- tf/main.tf
+++ tf/main.tf
@@ -34,17 +34,20 @@ provider "aws" {
 }
 
 resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
 
 }
 
 resource "aws_subnet" "subnet_a" {
   vpc_id            = aws_vpc.vpc.id
   availability_zone = "us-east-1a"
+  cidr_block        = "10.0.1.0/24"
 }
 
 resource "aws_subnet" "subnet_b" {
   vpc_id            = aws_vpc.vpc.id
   availability_zone = "us-east-1b"
+  cidr_block        = "10.0.2.0/24"
 }
 
 resource "aws_instance" "app_server" {
